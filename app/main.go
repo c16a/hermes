f19c421709fc22480653e50c7c2be2b01653fedc
Chaitanya Munukutla <chaitanya.m61292@gmail.com>
2020-12-23 22:10:43 +0530

    Added TLS for TCP server
    This is configurable via JSON/YAML config.
    
    Signed-off-by: Chaitanya Munukutla <chaitanya.m61292@gmail.com>

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"github.com/c16a/hermes/config"
 	"github.com/c16a/hermes/lib"
@@ -23,16 +24,36 @@ func main() {
 }
 
 func startTcpServer(serverConfig *config.Config, ctx *lib.ServerContext) {
+	var listener net.Listener
+	var listenerErr error
+
 	tcpAddress := serverConfig.Server.TcpAddress
-	l, err := net.Listen("tcp", tcpAddress)
-	if err != nil {
+
+	tlsConfigFromFile := serverConfig.Server.Tls
+	if tlsConfigFromFile == nil {
+		listener, listenerErr = net.Listen("tcp", tcpAddress)
+	} else {
+		if len(tlsConfigFromFile.CertFile) == 0 || len(tlsConfigFromFile.KeyFile) == 0 {
+			// TCP config invalid - don't start TCP server
+			return
+		}
+		cert, err := tls.LoadX509KeyPair(tlsConfigFromFile.CertFile, tlsConfigFromFile.KeyFile)
+		if err != nil {
+			// Could not read certs - don't start TCP server
+			return
+		}
+		tlsConfig := tls.Config{Certificates: []tls.Certificate{cert}}
+		listener, listenerErr = tls.Listen("tcp", tcpAddress, &tlsConfig)
+	}
+
+	if listenerErr != nil {
 		return
 	}
-	defer l.Close()
+	defer listener.Close()
 
 	fmt.Printf("Starting TCP server on %s\n", tcpAddress)
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			return
 		}
--- config/config.go
+++ config/config.go
@@ -5,6 +5,12 @@ type Config struct {
 }
 
 type Server struct {
+	Tls         *Tls   `json:"tls" yaml:"tls"`
 	TcpAddress  string `json:"tcp" yaml:"tcp"`
 	HttpAddress string `json:"http" yaml:"http"`
+}
+
+type Tls struct {
+	CertFile string `json:"cert" yaml:"cert"`
+	KeyFile  string `json:"key" yaml:"key"`
 }
