0aa482fe8cc80b9e2a90c4f8d24a86474c4bd38e
Chaitanya Munukutla <chaitanya.m61292@gmail.com>
2020-12-27 08:31:39 +0530

    Add offline support with server restart
    Badger cache is now persisted on disk, which retain any undelivered
    messages between server restarts and deliver when client connects later.
    
    Signed-off-by: Chaitanya Munukutla <chaitanya.m61292@gmail.com>

--- lib/config/config.go
+++ lib/config/config.go
@@ -7,11 +7,12 @@ type Config struct {
 
 // Server stores all server related configuration
 type Server struct {
-	Tls         *Tls   `json:"tls" yaml:"tls"`
-	TcpAddress  string `json:"tcp,omitempty" yaml:"tcp,omitempty"`
-	HttpAddress string `json:"http,omitempty" yaml:"http,omitempty"`
-	MaxQos      byte   `json:"max_qos,omitempty" yaml:"max_qos,omitempty"`
-	Auth        *Auth  `json:"auth,omitempty" yaml:"auth,omitempty"`
+	Tls         *Tls     `json:"tls" yaml:"tls"`
+	TcpAddress  string   `json:"tcp,omitempty" yaml:"tcp,omitempty"`
+	HttpAddress string   `json:"http,omitempty" yaml:"http,omitempty"`
+	MaxQos      byte     `json:"max_qos,omitempty" yaml:"max_qos,omitempty"`
+	Auth        *Auth    `json:"auth,omitempty" yaml:"auth,omitempty"`
+	Offline     *Offline `json:"offline,omitempty"`
 }
 
 // Tls stores the TLS config for the server
@@ -25,4 +26,10 @@ type Auth struct {
 	LdapHost string `json:"ldap_host,omitempty" yaml:"ldap_host,omitempty"`
 	LdapPort int    `json:"ldap_port,omitempty" yaml:"ldap_port,omitempty"`
 	LdapDn   string `json:"ldap_dn,omitempty" yaml:"ldap_dn,omitempty"`
+}
+
+type Offline struct {
+	Path         string `json:"path,omitempty"`
+	MaxTableSize int64  `json:"max_table_size,omitempty"`
+	NumTables    int    `json:"num_tables,omitempty"`
 }
